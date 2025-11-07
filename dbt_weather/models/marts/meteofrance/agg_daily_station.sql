513d05858dc652817ff84a0c9a94593abf2e01d7
martinezcoralie <martinezcoralie.mc@gmail.com>
2025-11-07 14:54:45 +0100

    fix(mart): correct column names

--- dbt_weather/models/marts/meteofrance/agg_daily_station.sql
+++ dbt_weather/models/marts/meteofrance/agg_daily_station.sql
@@ -1,9 +1,10 @@
 {{ config(materialized='table') }}
 select
   station_id,
+  station_name,
   date_trunc('day', validity_time_utc) as day_utc,
   avg(temperature_c) as avg_temp_c,
-  sum(precip_mm)    as sum_precip_mm,
-  max(wind_speed_ms) as max_wind_ms
+  sum(precip_mm_h)    as sum_precip_mm_h,
+  max(wind_speed_kmh) as max_wind_kmh
 from {{ ref('fct_obs_hourly') }}
-group by 1,2
+group by 1,2,3
