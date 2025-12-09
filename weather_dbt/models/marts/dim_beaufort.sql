532e3e562204db51ef3745c16472c33e2e7b609f
martinezcoralie <martinezcoralie.mc@gmail.com>
2025-12-09 13:48:59 +0100

    chore(models): Updated Beaufort dimension to use the simplified bins

--- weather_dbt/models/marts/dim_beaufort.sql
+++ weather_dbt/models/marts/dim_beaufort.sql
@@ -1,8 +1,13 @@
 {{ config(materialized='table') }}
 
-select
-    simple_level        as beaufort_level,
-    simple_label        as beaufort_label,
-    ms_min,
-    ms_max
-from {{ ref('beaufort_scale') }}
+with grouped as (
+    select
+        simple_level        as beaufort_level,
+        simple_label        as beaufort_label,
+        min(ms_min)         as ms_min,
+        max(ms_max)         as ms_max
+    from {{ ref('beaufort_scale') }}
+    group by 1, 2
+)
+
+select * from grouped
