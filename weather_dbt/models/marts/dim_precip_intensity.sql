165ef92d268b6d09a417229107f785faea267dbc
martinezcoralie <martinezcoralie.mc@gmail.com>
2026-01-26 14:28:45 +0100

    chore(dbt): use safe_cast macros in intensity dims

--- weather_dbt/models/marts/dim_precip_intensity.sql
+++ weather_dbt/models/marts/dim_precip_intensity.sql
@@ -2,7 +2,7 @@
 
 select
     cast(row_number() over (order by coalesce(max_mm, 9999), min_mm) as integer) as intensity_level,
-    min_mm,
-    max_mm,
+    {{ safe_double('min_mm') }} as min_mm,
+    {{ safe_double('max_mm') }} as max_mm,
     label as intensity_label
 from {{ ref('precip_intensity') }}
--- weather_dbt/models/marts/dim_temp_intensity.sql
+++ weather_dbt/models/marts/dim_temp_intensity.sql
@@ -2,7 +2,7 @@
 
 select
     cast(row_number() over (order by coalesce(max_c, 9999), min_c) as integer) as intensity_level,
-    min_c,
-    max_c,
+    {{ safe_double('min_c') }} as min_c,
+    {{ safe_double('max_c') }} as max_c,
     label as intensity_label
 from {{ ref('temp_intensity') }}
