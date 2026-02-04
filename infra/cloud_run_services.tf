8b90fc68a9a487e9813d3c89eb78ea72e8c2ef1d
martinezcoralie <martinezcoralie.mc@gmail.com>
2026-02-04 14:37:54 +0100

    feat(iac): import cloud run service streamlit (imported)

---
+++ infra/cloud_run_services.tf
@@ -0,0 +1,58 @@
+resource "google_cloud_run_v2_service" "streamlit" {
+  name     = "streamlit"
+  location = "europe-west1"
+  project  = "dbt-weather-poc"
+
+  ingress = "INGRESS_TRAFFIC_ALL"
+
+  template {
+    service_account = "cr-svc-streamlit@dbt-weather-poc.iam.gserviceaccount.com"
+
+    scaling {
+      min_instance_count = 0
+      max_instance_count = 3
+    }
+
+    timeout                          = "300s"
+    max_instance_request_concurrency = 80
+
+    containers {
+      name  = "streamlit-1"
+      image = "europe-west1-docker.pkg.dev/dbt-weather-poc/weather/streamlit@sha256:500f931c6bb2a725aea821c56929eeb20c32a4235d377653825c321337d60faf"
+
+      ports {
+        container_port = 8080
+      }
+
+      resources {
+        limits = {
+          cpu    = "1000m"
+          memory = "512Mi"
+        }
+      }
+
+      env {
+        name  = "BQ_DATASET"
+        value = "marts"
+      }
+      env {
+        name  = "BQ_PROJECT"
+        value = "dbt-weather-poc"
+      }
+      env {
+        name  = "BQ_TABLE"
+        value = "agg_station_latest"
+      }
+      env {
+        name  = "DATA_BACKEND"
+        value = "bigquery"
+      }
+    }
+  }
+
+  traffic {
+    type    = "TRAFFIC_TARGET_ALLOCATION_TYPE_LATEST"
+    percent = 100
+
+  }
+}
